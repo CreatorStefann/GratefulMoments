69320f3137468ebdbcda519f6c886c036f19d685
CreatorStefann <stefansandor26@gmail.com>
2025-11-29 14:22:53 +0200

    Initial Commit

---
+++ GratefulMoments/GratefulMomentsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GratefulMomentsApp.swift
+//  GratefulMoments
+//
+//  Created by Stefan Sandor on 29.11.2025.
+//
+
+import SwiftUI
+
+@main
+struct GratefulMomentsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
